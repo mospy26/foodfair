e09a5a288fb686fe688fe8840129e778800a03f9
T_Bot <fugfr6t@gmail.com>
2020-09-30 02:05:49 +1000

    Added Splash Screen

---
+++ app/src/main/java/com/foodfair/ui/miscellanous/FullscreenActivity.java
@@ -0,0 +1,54 @@
+package com.foodfair.ui.miscellanous;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import com.foodfair.MainActivity;
+import com.foodfair.R;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+    Context context = this;
+    Button button;
+
+    private final int SplashTimeOut = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            Intent intent = new Intent(context,MainActivity.class);
+
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+
+
+
+
+    }
+}
+
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
