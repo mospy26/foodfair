7c983fb553241a545eaa3b95a636e3ed78d1cf75
tofuwater <turk.dhit@gmail.com>
2020-10-31 13:18:28 +1100

    add leaderboard

--- app/src/main/java/com/foodfair/MainActivity.java
+++ app/src/main/java/com/foodfair/MainActivity.java
@@ -45,21 +45,6 @@ public class MainActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Sign out
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                /*if(item.getItemId() == R.id.nav_sign_out) {
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "TODO: Implement sign out", Toast.LENGTH_LONG);
-                    toast.show();
-                    //close navigation drawer
-                    DrawerLayout drawer = findViewById(R.id.drawer_layout);
-                    drawer.closeDrawer(GravityCompat.START);
-                }*/
-                return true;
-            }
-        });
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
---
+++ app/src/main/java/com/foodfair/ui/hamburger/LeaderboardAdapter.java
@@ -0,0 +1,75 @@
+package com.foodfair.ui.hamburger;
+
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.foodfair.databinding.AdapterLeaderboardBinding;
+import com.foodfair.model.Leaderboard;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class LeaderboardAdapter extends FirestoreAdapter<LeaderboardAdapter.ViewHolder> {
+
+    public interface OnLeaderboardSelectListener {
+        void onLeaderboardSelected(DocumentSnapshot leaderboard);
+    }
+
+    private OnLeaderboardSelectListener mListener;
+
+    private static final String TAG = "LeaderboardAdapter";
+
+    public LeaderboardAdapter(Query query, OnLeaderboardSelectListener listener){
+        super(query);
+        mListener = listener;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(AdapterLeaderboardBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(getSnapshot(position), mListener);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private AdapterLeaderboardBinding binding;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public ViewHolder(AdapterLeaderboardBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(final DocumentSnapshot snapshot,
+                         final OnLeaderboardSelectListener listener) {
+            Leaderboard leaderboard = snapshot.toObject(Leaderboard.class);
+            Resources resources = itemView.getResources();
+
+            Map<String, Object>[] rankingInfo = leaderboard.getRanking();
+
+            for(Map<String, Object> rankingDetail: rankingInfo){
+                binding.firstLine.setText(leaderboard.getRanking());
+            }
+
+        }
+    }
+}
