c3eee82e9af0588112b5bb615ba1380720dbbf25
devtsai <devtsai@outlook.com>
2020-10-31 19:57:02 +1100

    1. Add some utilities to convert bitmap to base64, base64 to bitmap.
    2. Compress the bitmap object to a specific size

---
+++ app/src/main/java/com/foodfair/utilities/Const.java
@@ -0,0 +1,11 @@
+package com.foodfair.utilities;
+
+public class Const {
+    public static final String FIREBASE_COLLECTION_USER_INFO = "users-info";
+    public static final String FIREBASE_COLLECTION_USER_INFO_KEY_PROFILE_IMAGE = "profile-image";
+    public static final String FIREBASE_COLLECTION_USER_INFO_KEY_NAME = "name";
+    public static final String FIREBASE_COLLECTION_USER_INFO_KEY_GENDER = "gender";
+    public static final Long FIREBASE_COLLECTION_USER_INFO_GENDER_VALUE_MALE = 1L;
+    public static final String FIREBASE_COLLECTION_USER_INFO_KEY_BIRTHDAY = "birthday";
+
+}
