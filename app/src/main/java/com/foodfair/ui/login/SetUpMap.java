2aa2879266d9db7cd869f199fc6c211f78fa5541
mospy26 <m.mustafaahmed@hotmail.com>
2020-11-07 15:42:19 +1100

    Sign up gets location of user

--- app/src/main/java/com/foodfair/ui/login/SetUpMap.java
+++ app/src/main/java/com/foodfair/ui/login/SetUpMap.java
@@ -1,15 +1,26 @@
 package com.foodfair.ui.login;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.foodfair.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,6 +29,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -26,19 +39,23 @@ import org.json.JSONObject;
 
 public class SetUpMap extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final int DEFAULT_ZOOM = 15;
+
     private GoogleMap mMap;
     private Marker mLastMarker;
     private LatLng prevPoint;
     private FloatingActionButton next;
+    LocationManager lm;
+    LocationResult locationResult;
+    Location currentLocation;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LatLng mDefaultLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setup__map);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
         next = findViewById(R.id.SetUpNext);
 
         next.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +72,18 @@ public class SetUpMap extends AppCompatActivity implements OnMapReadyCallback {
             }
         });
 
+        mDefaultLocation = new LatLng(-33.852, 151.211);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // Construct a FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLocation(this, locationResult);
+
         // Button that saves the location user chose
 //        Button saveButton;
 //        saveButton.setOnClickListener(new View.OnClickListener() {
@@ -67,11 +96,56 @@ public class SetUpMap extends AppCompatActivity implements OnMapReadyCallback {
 //        });
     }
 
+    private boolean getLocation(Context context, LocationResult result) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+        Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+        locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    // Obtain the current location of the device
+                    currentLocation = task.getResult();
+                    String currentOrDefault = "Current";
+
+                    if (currentLocation != null) {
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                new LatLng(currentLocation.getLatitude(),
+                                        currentLocation.getLongitude()), DEFAULT_ZOOM));
+
+                        prevPoint = new LatLng(currentLocation.getLatitude(),
+                                currentLocation.getLongitude());
+                        mMap.clear();
+                        mMap.addMarker(new MarkerOptions().draggable(true).position(new LatLng(currentLocation.getLatitude(),
+                                currentLocation.getLongitude())));
+
+                        // successfully retrieve user location
+
+
+                    } else {
+                        Log.d("Location GPS", "Current location is null. Using defaults.");
+                        currentOrDefault = "Default";
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+                        // Set current location to the default location
+
+                    }
+                }
+            }
+        });
+        return true;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng sydney = new LatLng(-33.852, 151.211);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
         googleMap.addMarker(new MarkerOptions()
-                .position(sydney)
+                .position(mDefaultLocation)
                 .title("Marker").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
         mMap = googleMap;
 
@@ -83,7 +157,7 @@ public class SetUpMap extends AppCompatActivity implements OnMapReadyCallback {
                 mLastMarker = mMap.addMarker(new MarkerOptions().draggable(true).position(latLng));
             }
         });
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, 12.0f));
 
 
 //        setContentView(R.layout.setup__map);
